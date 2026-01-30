9dbcce348fe803d0de59e8e08cee94372a7a7c5f
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-01-30 14:05:34 +0400

    fix

--- app.py
+++ app.py
@@ -860,7 +860,7 @@ Built with [Qwen3-TTS](https://github.com/QwenLM/Qwen3-TTS) by Alibaba Qwen Team
                     .then(lambda: (gr.update(interactive=True)),outputs=[design_btn])
                 post_btn.click(lambda: (gr.update(interactive=False)),outputs=[post_btn]) \
                     .then(postprocess,inputs=[design_audio_out,speed_factor_slider, silence_trimming, internal_silence_fix, unvoiced_removal],outputs=[post_output]) \
-                    .then(lambda: (gr.update(interactive=True),(gr.update(visible=True)),outputs=[post_btn,post_output])
+                    .then(lambda: (gr.update(interactive=True),gr.update(visible=True)),outputs=[post_btn,post_output])
             # Tab 2: Voice Clone (Base)
             with gr.Tab("Voice Clone (Base)"):
                 gr.Markdown("### Clone Voice from Reference Audio")
